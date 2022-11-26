7791208734f226e33c47ba88409476ab23f5a12c
Falguni thakkar <falgunithakkar663@gmail.com>
2022-11-26 11:29:17 +0530

    1st

---
+++ src/entity/Connection.java
@@ -0,0 +1,71 @@
+package entity;
+
+
+
+    public class Connection //implements InterfaceEms
+    {
+
+        private int id;
+        private int consumerId;
+        private int connectionTypeId;
+        private int oldReading;
+
+
+        public Connection() {
+            super();
+            // TODO Auto-generated constructor stub
+        }
+
+        public Connection(int id, int consumerId, int connectionTypeId, int oldReading) {
+            super();
+            this.id = id;
+            this.consumerId = consumerId;
+            this.connectionTypeId = connectionTypeId;
+            this.oldReading = oldReading;
+
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public int getConsumerId() {
+            return consumerId;
+        }
+
+        public void setConsumerId(int consumerId) {
+            this.consumerId = consumerId;
+        }
+
+        public int getConnectionTypeId() {
+            return connectionTypeId;
+        }
+
+        public void setConnectionTypeId(int connectionTypeId) {
+            this.connectionTypeId = connectionTypeId;
+        }
+
+        public int getOldReading() {
+            return oldReading;
+        }
+
+        public void setOldReading(int oldReading) {
+            this.oldReading = oldReading;
+        }
+
+            @Override
+            public String toString() {
+                return "Connection{" +
+                        "id=" + id +
+                        ", consumerId=" + consumerId +
+                        ", connectionTypeId=" + connectionTypeId +
+                        ", oldReading=" + oldReading +
+                        '}';
+            }
+    }
+
+
